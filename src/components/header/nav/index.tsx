a01c86edfc375ec8e5502751127c3cacdf618fb6
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-07-22 21:11:44 -0300

    feat: modal do navegation no header.tsx

--- src/components/header/nav/index.tsx
+++ src/components/header/nav/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import "./nav.css";
+import { FaGithub, FaLinkedin, FaX } from "react-icons/fa6";
 
 interface NavigationProps {
   scrollToMain: () => void;
@@ -15,6 +17,7 @@ export default function Navigation({
   scrollToContacts,
 }: NavigationProps) {
   const [width, setWidth] = React.useState(window.innerWidth);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   React.useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
@@ -25,7 +28,50 @@ export default function Navigation({
   }, []);
 
   if (width < 768) {
-    return <RxHamburgerMenu size={30} color="#fff" className="menu" />;
+    return (
+      <>
+        <RxHamburgerMenu
+          onClick={() => setIsOpen(!isOpen)}
+          size={30}
+          color="#fff"
+          className="menu"
+        />
+        <div className={isOpen ? "modal open" : "modal"}>
+          <FaX size={30} onClick={() => setIsOpen(!isOpen)} color="#fefefe" />
+          <nav>
+            <ul className="links">
+              <li className="link" onClick={scrollToMain}>
+                <span onClick={() => setIsOpen(!isOpen)}>Main</span>
+              </li>
+              <li className="link" onClick={scrollToAbout}>
+                <span onClick={() => setIsOpen(!isOpen)}>Portfólio</span>
+              </li>
+              <li className="link" onClick={scrollToProjects}>
+                <span onClick={() => setIsOpen(!isOpen)}>Meus Serviços</span>
+              </li>
+              <li className="link" onClick={scrollToContacts}>
+                <span onClick={() => setIsOpen(!isOpen)}>Contatos</span>
+              </li>
+            </ul>
+          </nav>
+          <div className="icons">
+            <a
+              href="https://www.linkedin.com/in/fabrício-oliveira-lopes-b713892bb"
+              className="icon"
+            >
+              <FaLinkedin size={30} />
+            </a>
+            <a href="https://github.com/Fabrioco" className="icon">
+              <FaGithub size={30} />
+            </a>
+          </div>
+          <div className="job">
+            <p>Fabriciooliveiralopes50@gmail.com</p>
+            <p>+55 11 96016-8159</p>
+          </div>
+        </div>
+      </>
+    );
   }
 
   return (
